eab4fa2968d0a61b055ff25613fe90ced8339dc7
Jorge Alvarado Diaz de Leon <75354944+Jorge1x10@users.noreply.github.com>
2026-08-29 22:00:29 -0600

    Remove TODO comments from PrivacyPolicyPage
    Removed TODO comments regarding real data replacement.

--- client/eniu/src/modules/Legal/pages/PrivacyPolicyPage.jsx
+++ client/eniu/src/modules/Legal/pages/PrivacyPolicyPage.jsx
@@ -1,8 +1,5 @@
 import { Link } from "react-router";
 
-// TODO: sustituir por los datos reales antes de publicar la política.
-// Son los tres únicos huecos del documento; el resto describe lo que el
-// sistema hace de verdad.
 const RESPONSABLE = "Jorge Arturo Alvarado Díaz de León";
 const CONTACTO = "hola@eniu.app";
 const DOMICILIO = "Paseo del Origen 600, Club de Golf Santa Anita, Tlajomulco de Zúñiga, Jal., C.P. 45645";
